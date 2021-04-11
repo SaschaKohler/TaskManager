f8da606e5ad2f39170034cbecb581a1b1435655a
sascha.kohler <sascha.kohler@sent.at>
2021-04-11 09:42:37 +0200

    ./node_modules

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  Designation:string;
+  Username:string;
+  NoOfTeamMembers:number;
+  TotalCostOfAllProjects:number;
+  PendingTasks:number;
+  UpComingProjects:number;
+  ProjectsCost:number;
+
+  CurrentExpenditure:number;
+  AvailableFunds:number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.Designation="Team Leader";
+    this.Username="John Smith";
+    this.NoOfTeamMembers=67;
+    this.TotalCostOfAllProjects=240;
+    this.PendingTasks=15;
+    this.UpComingProjects=2;
+    this.ProjectsCost=21113507;
+    this.CurrentExpenditure=96788;
+    this.AvailableFunds=52;
   }
 
 }
